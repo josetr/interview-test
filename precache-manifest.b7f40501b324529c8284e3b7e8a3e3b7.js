5fa311826aa3fed754467349fde5aa78a92d9a26
josetr <37419832+josetr@users.noreply.github.com>
2022-02-04 13:00:07 +0000

    Updates

---
+++ precache-manifest.b7f40501b324529c8284e3b7e8a3e3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0600f4260a9fb3d51435",
+    "url": "/static/css/main.9f6b3fc6.chunk.css"
+  },
+  {
+    "revision": "0600f4260a9fb3d51435",
+    "url": "/static/js/main.fe81d186.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64fc2570b8945d73db76",
+    "url": "/static/js/2.2e6c614c.chunk.js"
+  },
+  {
+    "revision": "93a08c30d7705c7414d48ae3490db4f0",
+    "url": "/index.html"
+  }
+];
